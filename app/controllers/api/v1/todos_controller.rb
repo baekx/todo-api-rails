2fed4784469b948740de64d89bc7a54d243b0d0c
seo <seungbaek.seo@spacemarket.co.jp>
2023-09-01 16:22:27 +0900

    todos_controllerにcreateメソッド作成

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,10 +1,28 @@
 module Api
-    module V1
-        class TodosController < ApplicationController
-            def index
-                todos = Todo.all
-                render status: 200, json: { todos: todos }
-            end
-        end
-    end
+	module V1
+		class TodosController < ApplicationController
+			def index
+				todos = Todo.all
+				render status: 200, json: { todos: todos }
+			end
+
+			def new
+				todo = Todo.new
+			end
+
+			def create
+				todo = Todo.new(todo_params)
+				if todo.save
+					render status: 200, json: { todo: todo }
+				else
+					render status: 400, json: { data: todo.errors }
+				end
+			end
+
+			private
+				def todo_params
+					params.permit(:title, :user_id)
+				end
+		end
+	end
 end
